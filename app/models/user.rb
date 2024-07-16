167b62f277ee825989653d1d4014cc70061ab4a1
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-16 08:38:42 +0000

    [Add]ヘッダーの追加、サインアップ・ログイン・ログアウト機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+
+  def get_profile_image
+    profile_image.variant(resize_to_lomit: [width, height]).processd
+  end
 end
