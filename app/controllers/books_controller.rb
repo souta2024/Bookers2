03afa45d2c845c658fd6b49d19294a0272225f91
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-17 13:23:22 +0000

    [Add]/booksにuserinfoとnewbookの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+  def index
+    @user_id = current_user.id
+    @user = User.find(@user_id)
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user.id = current_user.id
+    @book.save
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   def get_profile_image
-    profile_image.variant(resize_to_lomit: [width, height]).processd
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
