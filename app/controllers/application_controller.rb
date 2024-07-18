7002af1f12153db970c5a58a9ba3b5cff22e9c09
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-18 10:55:27 +0000

    [Fix]user/:id/editページのUpdate BookをUpdate Userへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
     @id = current_user.id
     user_path([@id])
   end
