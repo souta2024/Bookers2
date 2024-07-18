338b8c4b103acb7934fd6bf3ec17986fd2cfc806
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-18 08:13:24 +0000

    [Add]book_editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,22 @@ class BooksController < ApplicationController
     @book_new = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
