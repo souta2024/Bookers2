a32dd1076b8647735b2865f635bc16c5ecc867cf
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-18 06:29:56 +0000

    [Add]/usersページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.find(params[:id])
+    @user_id = @book.user_id
+    @user = User.find(@user_id)
+    @book_new = Book.new
   end
 
   def index
+    @user_id = current_user.id
+    @user = User.find(@user_id)
+    @book_new = Book.new
+    @users = User.all
   end
 
   def edit
