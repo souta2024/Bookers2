f6d5a77a00cce7a1a64fc7095063ee274b6a4259
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-07-18 10:41:21 +0000

    [Add]/user/:id/editページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books, only: [:new, :create,:index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :index, :edit,]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
